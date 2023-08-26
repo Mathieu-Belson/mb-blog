cd685ec0a126a9e3fb167fd7bb46779302448eb2
mathieu belson <mathieu.belson@gmail.com>
2023-08-26 23:30:50 +0900

    first hook added

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 const Home = () => {
+    // let name = 'Mario';
+
+    const handleClick = () => {  
+  
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p> { name }</p>
+            <button onClick={handleClick}>Click me</button>
         </div>
      );
 }
- 
-export default Home;
+
+export default Home;  
